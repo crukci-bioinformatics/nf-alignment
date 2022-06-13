96179ae19c12838491347e996277f7495489df22
Richard Bowers <richard.bowers@cruk.cam.ac.uk>
2022-06-13 17:22:03 +0100

    Build scripts updated. Create singularity script from DockerFile.

--- container/makedocker.sh
+++ container/makedocker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sudo docker build --tag "crukcibioinformatics/alignment:40.2" --file Dockerfile .
-docker push crukcibioinformatics/alignment:40.2
+sudo docker push crukcibioinformatics/alignment:40.2
--- container/makesingularity.sh
+++ container/makesingularity.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
+if [[ -d spython ]]
+then
+    source spython/bin/activate
+else
+    python3 -m venv spython
+    source spython/bin/activate
+    pip install spython
+fi
+
+spython recipe Dockerfile > singularity_spec.txt
+
 sudo singularity build alignment40.2.sif singularity_spec.txt
 chmod a-x alignment40.2.sif
+
