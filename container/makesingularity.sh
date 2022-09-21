d91ce8884cc78f60aea3f0880d71a143382f5463
Richard Bowers <richard.bowers@cruk.cam.ac.uk>
2022-09-21 14:13:29 +0100

    Change owner after image build.

--- container/makesingularity.sh
+++ container/makesingularity.sh
@@ -3,8 +3,9 @@
 TAG="4.0.4"
 REPO="crukcibioinformatics/alignment:$TAG"
 
-sudo rm -rf alignment-${TAG}.sif
+sudo rm -f alignment-${TAG}.sif
 
 sudo singularity build alignment-${TAG}.sif docker-daemon://${REPO}
+sudo chown $USER alignment-${TAG}.sif
 chmod a-x alignment-${TAG}.sif
 
